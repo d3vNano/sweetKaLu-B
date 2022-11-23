b2a7a87d663650ef3096c93aa6e28369058e7f0a
dorigoluiz <luizdorigo01@gmail.com>
2022-11-23 14:14:58 -0300

    feat: base product routes/controller/schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import chalk from "chalk";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
