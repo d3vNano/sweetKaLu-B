2c63cc19f71f51c6d7ee226860dc76379cd581db
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-24 19:48:57 -0300

    fix: remove sessionsCollection

--- src/database/collections.js
+++ src/database/collections.js
@@ -1,13 +1,7 @@
 import db from "./db.js";
 
 const usersCollection = db.collection("users");
-const sessionsCollection = db.collection("sessions");
 const productsCollection = db.collection("products");
 const cartsCollection = db.collection("carts");
 
-export {
-    usersCollection,
-    sessionsCollection,
-    productsCollection,
-    cartsCollection,
-};
+export { usersCollection, productsCollection, cartsCollection };
