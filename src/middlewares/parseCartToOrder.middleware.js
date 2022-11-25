1fbbd64eda5dd594d05ed0902c8f199df5ed97ac
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-25 16:33:00 -0300

    feat: add GET /checkout with validations

--- src/middlewares/parseCartToOrder.middleware.js
+++ src/middlewares/parseCartToOrder.middleware.js
@@ -1,38 +1,18 @@
-import { ObjectId } from "mongodb";
-import { cartsCollection } from "../database/collections.js";
-
 export async function parseCartToOrder(req, res, next) {
-    const { id } = req.params;
-    const user = req.user;
+    const cart = req.cart;
 
     try {
-        const cart = await cartsCollection.findOne({
-            _id: new ObjectId(id),
-            userId: user.id,
-            status: "opened",
-        });
-        if (!cart) {
-            return res.status(404).send({
-                message: "Carrinho não encontrado ou pertence a outro usuário",
-            });
-        }
-
-        cart.totalItens = 0;
-        cart.subtotalPrice = 0;
-        cart.products.forEach((e) => {
-            cart.totalItens += e.stockToReserve;
-            cart.subtotalPrice += e.price * e.stockToReserve;
-        });
-
         const deliveryFee = 10;
         const totalPrice = cart.subtotalPrice + deliveryFee;
 
-        const cartOrder = {
+        const order = {
+            userId: cart.userId,
+            cartId: cart._id,
+            subtotalPrice: cart.subtotalPrice,
             deliveryFee,
             totalPrice,
-            ...cart,
         };
-        req.cart = cartOrder;
+        req.order = order;
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
