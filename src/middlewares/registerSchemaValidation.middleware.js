962f4fff37c8918599c069e3c011209c4198d4b4
Victor Azevedo <josevcazevedo@gmail.com>
2023-06-26 10:56:04 -0300

    feat: remove repeatPassword

--- src/middlewares/registerSchemaValidation.middleware.js
+++ src/middlewares/registerSchemaValidation.middleware.js
@@ -3,23 +3,22 @@ import dayjs from "dayjs";
 import { registerSchema } from "../models/users.models.js";
 
 export function registerSchemaValidation(req, res, next) {
-    const { username, email, phone, password, repeatPassword } = req.body;
-    const newUser = { username, email, phone, password, repeatPassword };
+  const { username, email, phone, password } = req.body;
+  const newUser = { username, email, phone, password };
 
-    const { error } = registerSchema.validate(newUser, { abortEarly: false });
-    if (error) {
-        const message = error.details.map((detail) => detail.message);
-        console.log(
-            chalk.magentaBright(
-                dayjs().format("YYYY-MM-DD HH:mm:ss"),
-                "- BAD_REQUEST:",
-                message
-            )
-        );
-        res.status(422).send({ message });
-        return;
-    }
-    delete newUser.repeatPassword;
-    req.newUser = newUser;
-    next();
+  const { error } = registerSchema.validate(newUser, { abortEarly: false });
+  if (error) {
+    const message = error.details.map((detail) => detail.message);
+    console.log(
+      chalk.magentaBright(
+        dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        "- BAD_REQUEST:",
+        message
+      )
+    );
+    res.status(422).send({ message });
+    return;
+  }
+  req.newUser = newUser;
+  next();
 }
--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,19 +1,18 @@
 import joi from "joi";
 
 export const registerSchema = joi.object({
-    username: joi.string().min(3).max(20).required(),
-    email: joi.string().email().required(),
-    phone: joi
-        .string()
-        .min(10)
-        .max(11)
-        .pattern(/^[0-9]+$/)
-        .required(),
-    password: joi.string().required().min(6).max(12),
-    repeatPassword: joi.ref("password"),
+  username: joi.string().min(3).max(20).required(),
+  email: joi.string().email().required(),
+  phone: joi
+    .string()
+    .min(10)
+    .max(11)
+    .pattern(/^[0-9]+$/)
+    .required(),
+  password: joi.string().required().min(6).max(12),
 });
 
 export const loginSchema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().required().min(6).max(12),
+  email: joi.string().email().required(),
+  password: joi.string().required().min(6).max(12),
 });
